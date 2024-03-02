9523538859ac50f4bd61aa286c31c889154b7f7c
DavidPizza33 <146900501+DavidPizza33@users.noreply.github.com>
2024-03-02 12:23:17 -0500

    Creando la estructura del API

---
+++ Clases-Software/Software4/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace Software4.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
